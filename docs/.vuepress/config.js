83d17659800b6d53016d38c65f97c208942a44ab
uchidahr <uchidahr@nousoku.jp>
2019-05-22 20:06:18 +0900

    Vuepressのバージョンを1.xに変更。GAの設定方法を変更。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,9 +2,12 @@ module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
     serviceWorker: true,
-    plugins: {
-        '@vuepress/google-analytics': {
-            ga: 'UA-140337981-1'
-        }
-    }
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-140337981-1'
+            }
+        ]
+    ]
 }
