6eb7bc42165e619fdab7f6087ae8fd7e98ed25e4
uchidahr <uchidahr@nousoku.jp>
2019-07-22 18:34:12 +0900

    サイト名とURL、キーワードを変更。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    title: '電脳自由研究室（仮）',
+    title: 'ucchies.com',
     description: 'エンジニアによるブログ。システム開発業務や資格試験、転職活動などについて書きます。',
     serviceWorker: true,
     head: [
         //['link', { rel: 'icon', type: 'image/png', href: '/favicon.png'}],
-        ['meta', { name: 'keywords', content: 'システム開発, SI, SES, IT, IT技術, Web, 転職, 資格, ' +
-                'ブラック企業, 無職, ニート, 引きこもり'}],
+        ['meta', { name: 'keywords', content: 'システム開発, SI, SES, IT, IT技術, Web, 転職, 資格, アプリ開発'}],
         ['meta', { name: "og:title", content: this.title}],
         ['meta', { name: "og:description", content: this.description}],
         ['meta', { name: "og:type", content: "website"}],
-        ['meta', { name: "og:url", content: "https://freedom.kaiba.io/"}],
+        ['meta', { name: "og:url", content: "https://ucchies.com/"}],
         ['meta', { name: "og:locale", content: "ja_JP"}]
     ],
     plugins: [
